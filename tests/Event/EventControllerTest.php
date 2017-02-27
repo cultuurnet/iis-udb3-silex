d7581190e5f405ad72477dbd32b870eef046365e
Luc Wollants <luc.wollants@apptite.be>
2017-02-27 20:58:22 +0100

    III-1163 Implemented unit tests.

--- tests/Event/EventControllerTest.php
+++ tests/Event/EventControllerTest.php
@@ -3,6 +3,8 @@
 namespace CultuurNet\UDB3\IIS\Silex\Event;
 
 use CultuurNet\UDB3\IISStore\Stores\RepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use ValueObjects\Identity\UUID;
 
 class EventControllerTest extends \PHPUnit_Framework_TestCase
 {
@@ -30,7 +32,25 @@ class EventControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function it_returns_event_xml()
     {
-        $this->assertTrue(false);
+        $uuid = new UUID();
+        $eventXml = '<xml>test</xml>';
+
+        $this->repository->expects($this->once())
+            ->method('getEventXml')
+            ->with($uuid->toNative())
+            ->willReturn($eventXml);
+
+        $expectedResponse = new Response(
+            $eventXml,
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/xml',
+            ]
+        );
+
+        $response = $this->eventController->get($uuid->toNative());
+
+        $this->assertEquals($expectedResponse, $response);
     }
 
     /**
@@ -38,7 +58,16 @@ class EventControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function it_returns_bad_request_on_invalid_event_cdbid()
     {
-        $this->assertTrue(false);
+        $eventCdbid = 'd4508db3-46d5-415a-b288-a64327eda3e@';
+
+        $expectedResponse = new Response(
+            'Could not convert ' . $eventCdbid . ' to valid UUID.',
+            Response::HTTP_BAD_REQUEST
+        );
+
+        $response = $this->eventController->get($eventCdbid);
+
+        $this->assertEquals($expectedResponse, $response);
     }
 
     /**
@@ -46,6 +75,20 @@ class EventControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function it_returns_not_found_on_unknown_event_cdbid()
     {
-        $this->assertTrue(false);
+        $uuid = new UUID();
+
+        $this->repository->expects($this->once())
+            ->method('getEventXml')
+            ->with($uuid->toNative())
+            ->willReturn(null);
+
+        $expectedResponse = new Response(
+            'Event with id ' . $uuid->toNative() . ' not found.',
+            Response::HTTP_NOT_FOUND
+        );
+
+        $response = $this->eventController->get($uuid->toNative());
+
+        $this->assertEquals($expectedResponse, $response);
     }
 }
